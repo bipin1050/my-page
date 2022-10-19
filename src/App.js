58d0ff39ae6f3586d35eaff84755ab7b9f1713cb
bipin1050 <bipin.khanal1050@gmail.com>
2022-10-19 23:30:09 +0545

    fresh

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="App">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque, doloremque. Laboriosam sit praesentium ad blanditiis dolores numquam molestias veritatis illo iusto debitis non exercitationem dignissimos, harum labore aut, perferendis adipisci?
+    </div>
+  );
+}
+
+export default App;
