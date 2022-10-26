cfadda01e3c7a501cc5f568c46a4a9cc4b490459
bipin1050 <bipin.khanal1050@gmail.com>
2022-10-26 15:51:02 +0545

    img fixed

--- src/components/footer.js
+++ src/components/footer.js
@@ -10,10 +10,10 @@ const Footer = () => {
         <footer class="footer py-3">
             <div class="container">
                 <p className='medium mb-0 text-light'>Social Links</p>
-                <a href='https://facebook.com/bipin.khanal1050' target='_blank'><img src={facebook} width='30px' /> </a>
-                <a href='https://www.instagram.com/_bip_in_/' target='_blank'><img src={instagram} width='30px' /> </a>
-                <a href='https://twitter.com/bpin_khanal' target='_blank'><img src={twitter} width='30px' /> </a>
-                <a href='https://www.linkedin.com/in/bipin-khanal-66a082244/' target='_blank'><img src={linkedin} width='30px' /> </a>
+                <a href='https://facebook.com/bipin.khanal1050' target='_blank' rel="noreferrer"><img src={facebook} width='30px' alt='facebook'/> </a>
+                <a href='https://www.instagram.com/_bip_in_/' target='_blank' rel="noreferrer"><img src={instagram} width='30px'alt='instagram' /> </a>
+                <a href='https://twitter.com/bpin_khanal' target='_blank' rel="noreferrer"><img src={twitter} width='30px' alt='twitter'/> </a>
+                <a href='https://www.linkedin.com/in/bipin-khanal-66a082244/' target='_blank' rel="noreferrer"><img src={linkedin} width='30px' alt='linkedin'/> </a>
             </div>
             <br />
             <p className='container medium mb-0 text-light'>&copy; 2022, Bipin</p>
